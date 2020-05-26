7b449158f086f5349cf0ebb7d26e827d6c75391d
Mona Parnian <m.parnian.eng@gmail.com>
2020-05-26 18:04:07 +1000

    first version of trello has been build includes drag and drop

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Bootstrap has been used to style the app
 1- Jumbotorn to style the header
 2- List groups to style list of aadings
 3- Input group to style input 
+
+Using Jquery and javascript to design how cards work
+1- have a click event and through this even, we capture data entry and append it to the card
+2- at the same time, we add the entry value to the first card array
+3- used draggable jquery to make each entry draggable
+4- used droppable jquery to be able to drop dragged item into droppable place
+5- at the same time we drop an card item, item is deleted from first card array and pasted into second card array.
--- app.js
+++ app.js
@@ -1,18 +1,50 @@
 $(()=> {
 
+    const firstCardData = [];
+    const secondCardData = [];
 
     function moveItemToList(){
         event.preventDefault();
         const dataEntry = $('#dataEntry').val();
-        console.log(dataEntry);
-        const dataEntryList = $(`<li class="list-group-item">${dataEntry}</li>`);
+        firstCardData.push(dataEntry);
+        console.log(firstCardData);
+
+        const dataEntryList = $(`<div class='ui-widget-content draggable'><li class="list-group-item">${dataEntry}</li></div>`);
         $('#firstList').append(dataEntryList);
         $('input').val('');
+
+        $('.draggable').draggable();
+        $('.draggable').draggable({ revert: true });
+        $("#droppable").droppable({
+            drop: ( event, ui )=> {
+            //   console.log(event);
+            //   console.log(ui);
+
+            //get the value of the dragged item
+            const dragItemContent = $(ui.draggable.html()).text();
+            console.log(dragItemContent);
+
+            //delete a dragged element from a first card array
+            const index = firstCardData.indexOf(dragItemContent);
+            firstCardData.splice(index,1);
+
+            //add a dragged element to second card array
+            secondCardData.push(dragItemContent);
+
+            console.log('first array',firstCardData);
+            console.log('second array', secondCardData);
+
+            //remove style from dragged item to ensure it sits in proper position and prepend it to the droppable list
+                ui.draggable.removeAttr('style');
+              $("#secondList").prepend(ui.draggable);
+
+            }
+          });
     }
 
-
-
     $('#button-addon2').on('click',moveItemToList);
-    
+
+    // $('#dragging').draggable();
+
 
 })

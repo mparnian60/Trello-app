1cfa1a04f1b41bb9fb9fee75aadc102918d7776d
Mona Parnian <m.parnian.eng@gmail.com>
2020-05-26 12:04:36 +1000

    add button works and adds entry to the first list

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 GA project
 Step 1-
 prepare the html and get the visual side of the project ready
+
+Bootstrap has been used to style the app
+1- Jumbotorn to style the header
+2- List groups to style list of aadings
+3- Input group to style input 
--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 $(()=> {
-    console.log('ready to go!');
+
+
+    function moveItemToList(){
+        event.preventDefault();
+        const dataEntry = $('#dataEntry').val();
+        console.log(dataEntry);
+        const dataEntryList = $(`<li class="list-group-item">${dataEntry}</li>`);
+        $('#firstList').append(dataEntryList);
+        $('input').val('');
+    }
+
+
+
+    $('#button-addon2').on('click',moveItemToList);
+    
+
 })

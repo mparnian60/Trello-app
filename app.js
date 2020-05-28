b54bebcdba51a3d373c4336827afa94819a0421f
Mona Parnian <m.parnian.eng@gmail.com>
2020-05-28 21:33:04 +1000

    dynamic elements with drag and drop

--- app.js
+++ app.js
@@ -1,50 +1,153 @@
-$(()=> {
+$(() => {
 
-    const firstCardData = [];
-    const secondCardData = [];
+  const cardData = [];
+  const tableIndex = [];
+  let tableIndexNum;
+  let buttonClass = 'button-addon2';
 
-    function moveItemToList(){
-        event.preventDefault();
-        const dataEntry = $('#dataEntry').val();
-        firstCardData.push(dataEntry);
-        console.log(firstCardData);
 
-        const dataEntryList = $(`<div class='ui-widget-content draggable'><li class="list-group-item">${dataEntry}</li></div>`);
-        $('#firstList').append(dataEntryList);
-        $('input').val('');
+  function drawTableToDom(e) {
 
-        $('.draggable').draggable();
-        $('.draggable').draggable({ revert: true });
-        $("#droppable").droppable({
-            drop: ( event, ui )=> {
-            //   console.log(event);
-            //   console.log(ui);
+    // console.log('event',e);
+    const ListName = $('#listNameEntry').val();
+    tableIndex.push({ tableNmae: ListName })
+    tableIndexNum = tableIndex.length - 1;
 
-            //get the value of the dragged item
-            const dragItemContent = $(ui.draggable.html()).text();
-            console.log(dragItemContent);
 
-            //delete a dragged element from a first card array
-            const index = firstCardData.indexOf(dragItemContent);
-            firstCardData.splice(index,1);
+    const list = $(`<div class="col-sm-6 table droppable" data-column=${tableIndexNum}>
+          <div class='dropZone'>
+              <div class="card" style="width: 18rem;">
+                  <div class="card-header">
+                  ${ListName}
+                  </div>
+                  <ul  id ='list${tableIndexNum}' class="list-group list-group-flush">
+                  
+                  </ul>
+              </div>
+              <div class="input-group" style="width: 18rem;">
+                  <input type="text" id='dataEntry${tableIndexNum}' class="form-control" placeholder="Add a card" aria-label="Recipient's username" aria-describedby="button-addon2">
+                  <div class="input-group-append">
+                  <button class="btn btn-outline-success addItem" type="button" data-column=${tableIndexNum}>Add</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+    `);
 
-            //add a dragged element to second card array
-            secondCardData.push(dragItemContent);
+    $('.row').append(list)
+//make list entry empty
+    $('input').val('');
 
-            console.log('first array',firstCardData);
-            console.log('second array', secondCardData);
+    // $('.droppable').droppable({
+    //   drop: (event, ui) => {
+    //     console.log(event);
+    //     console.log(ui);
 
-            //remove style from dragged item to ensure it sits in proper position and prepend it to the droppable list
-                ui.draggable.removeAttr('style');
-              $("#secondList").prepend(ui.draggable);
+    //     //get the data of the dragged item
+    //     let dragItemContent = $(ui.draggable[0].dataset);
 
-            }
-          });
-    }
+    //     // Since HTMLElement.dataset returns a DOM string map, 
+    //     // the only way I found is to convert it into an native object by using below code
+    //     dragItemContent = JSON.parse(JSON.stringify(dragItemContent));
 
-    $('#button-addon2').on('click',moveItemToList);
+    //     //get index number of dragged item
+    //     const indexNum = dragItemContent[0].index;
+    //     cardData[indexNum].status = 1;
+    //     // console.log(cardData);
 
-    // $('#dragging').draggable();
+    //     //remove style from dragged item to ensure it sits in proper position and prepend it to the droppable list
+    //     // ui.draggable.removeAttr('style');
+    //     // $("#firstList").append(ui.draggable);
+    //     $(`#list${tableIndexNum}`).prepend(ui.draggable);
+
+
+    //   }
+    // });
+
+
+  }
+
+  $('#button-addon').on('click', drawTableToDom);
+
+
+
+
+  // const boardName = prompt('Name your board','Add board Title');
+  // $('.board-name').append(boardName);
+
+  function moveItemToList(e) {
+    // console.log(e);
+    // console.log(e.target.dataset.column);
+
+    event.preventDefault();
+    const dataEntry = $(`#dataEntry${e.target.dataset.column}`).val();
+    cardData.push({ title: dataEntry, status: e.target.dataset.column });
+    const dataIndexNum = cardData.length - 1;
+    $('input').val('');
+    // console.log(dataIndexNum);
+    // console.log(cardData);
+
+    const dataEntryList = $(`<div class='ui-widget-content draggable' data-index=${dataIndexNum}><li class="list-group-item">${dataEntry}</li></div>`);
+    $(`#list${e.target.dataset.column}`).append(dataEntryList);
+
+    $('.draggable').draggable();
+    $('.draggable').draggable({ revert: true });
+
+    // $('.droppable').droppable({
+    //   drop: (event, ui) => {
+    //     console.log(event);
+    //     console.log(ui);
+
+    //     //get the data of the dragged item
+    //     let dragItemContent = $(ui.draggable[0].dataset);
+
+    //     // Since HTMLElement.dataset returns a DOM string map, 
+    //     // the only way I found is to convert it into an native object by using below code
+    //     dragItemContent = JSON.parse(JSON.stringify(dragItemContent));
+
+    //     //get index number of dragged item
+    //     const indexNum = dragItemContent[0].index;
+    //     cardData[indexNum].status = 1;
+    //     // console.log(cardData);
+
+    //     //remove style from dragged item to ensure it sits in proper position and prepend it to the droppable list
+    //     // ui.draggable.removeAttr('style');
+    //     // $("#firstList").append(ui.draggable);
+    //     $("#list1").prepend(ui.draggable);
+
+
+    //   }
+    // });
+
+    $('.droppable').droppable({
+      drop: (event, ui) => {
+        console.log(event);
+        console.log(ui);
+
+        // const columnTarget = e.target.dataset.column;
+        console.log('column target', e.target.dataset.column);
+
+        //get the data of the dragged item
+        let dragItemContent = $(ui.draggable[0].dataset);
+
+        // Since HTMLElement.dataset returns a DOM string map, 
+        // the only way I found is to convert it into an native object by using below code
+        dragItemContent = JSON.parse(JSON.stringify(dragItemContent));
+
+        //get index number of dragged item
+        const indexNum = dragItemContent[0].index;
+        cardData[indexNum].status = 0;
+        // console.log(cardData);
+
+        //remove style from dragged item to ensure it sits in proper position and prepend it to the droppable list
+        $(`#list${e.target.dataset.column}`).prepend(ui.draggable);
+
+
+      }
+    });
+  }
+
+  $(document).on("click", '.addItem', moveItemToList);
 
 
 })

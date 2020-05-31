db1943f04bba0846f2b0b12c80007480d0d98207
Mona Parnian <m.parnian.eng@gmail.com>
2020-05-31 12:05:52 +1000

    Dropping doesn't work on relaoded lists

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Trello-app
+# 
 GA project
 Step 1-
 prepare the html and get the visual side of the project ready
@@ -14,3 +14,39 @@ Using Jquery and javascript to design how cards work
 3- used draggable jquery to make each entry draggable
 4- used droppable jquery to be able to drop dragged item into droppable place
 5- at the same time we drop an card item, item is deleted from first card array and pasted into second card array.
+
+# PROJECT #1 - Trello-app
+## Project Overview
+The Overview of this project was to create a simple Trello app to be used to organizes and manage projects/tasks. Making different cards anable user to be able to track tasks progress in terms of what's the list of tasks that needs to be done, which task is in prgress and whwihc one has been completed. 
+Imagine a white board, filled with lists of sticky notes, with each note as a task.
+## TECHNOLOGIES & RESOURCES USED
+* HTML5
+* CSS
+* Javascript
+* Jquery
+* Bootstrap
+* JSON
+## CORE REQUIREMENTS
+The Core requirements set out for this project where the following:
+* **Models.** A minimum of 3 models with appropriate associations.
+* **Views.** Use partial views to avoid DRY practices.
+* **Handles invalid data.** Appropriate form handlers to validate data or notify users of invalid data.
+* **Use Gems** Include Ruby Gems that utilise API Calls.
+* **User Login.** Basic User Authentication
+## PLANNING & DELIVERABLES
+* **Scope -** AIM: The aim of the project was to build a simple portfolio and stock tracker for the user.
+Features: Stock API Call for live data, portfolio customisation, news article search and save for user to re-read, time-series graphs.
+Implementation Period: 2 Weeks
+## CHALLENGE ADD-ONS (Achieved)
+* Using data attributes to be able to recognize item's status in diffrent table after dropping.
+* Learned how to create local data storage and how to load it to Dom.
+## PROBLEMS FACED
+* I didn't know how to bind click event to ('.addItem') class as it was created static through app.js not index.HTML. I finally ended up using event delegation.By delegating an event handler, I could ensure it is executed for ('.addItem') class that did not exist in the DOM at the time of binding. This way the event handler will actually be attached to something and can be executed as appropriate later on.
+* 
+* JQUERY Framework Conflicts - due to the use of Twitter Bootstrap and the additional complexity of Chartkick, the charts were not displaying correctly hence were removed.
+* Lack of knowledge with user models - I feel i did not have a good enough understanding of users/sessions to be able to associate them correctly to the portfolios.
+## FURTHER IMPROVEMENTS
+* Implement live charts into dashboard view.
+* Implement correct association for news and stocks
+* Implement correct association for users and portfolios
+* Implement autocomplete forms for news and stocks search
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let cardData = [];
+let tableEntry = [];
+let items = [];
 let tableIndex = [];
 let tableIndexNum;
 let columns;
-let items;
 let ListName;
 
 //Naming board
@@ -10,61 +10,68 @@ const boardName = prompt('Add board Title');
 $('.board-name').append(boardName);
 
 //load saved Trello-app into browser
-function loadTrelloApp(){
-  
-    columns = window.localStorage.getItem('Trello-app-column');
-    items = window.localStorage.getItem('Trello-app-items');
+function loadTrelloApp() {
 
-    columns = JSON.parse(columns);
-    items = JSON.parse(items);
+  columns = window.localStorage.getItem('Trello-app-column');
+  items = window.localStorage.getItem('Trello-app-items');
 
-    //if columns has something init or the array length is not zero
-    if(columns && columns.length){
-      loadTableNameToDom(columns);
-    }
+  columns = JSON.parse(columns);
+  items = JSON.parse(items);
 
-    console.log('columns',columns)
-    console.log('item',items);
-    
+  //if columns has something init or the array length is not zero
+  if (columns && columns.length) {
+    loadTableNameToDom(columns);
+  }
+
+  console.log('columns', columns)
+  console.log('item', items);
+
 }
 
 function loadTableNameToDom(columns) {
 
-    columns.forEach((element,index) => {
-      const ListName = element.tableName;
-      console.log(ListName);
-  
-    createList(ListName,index);
+  columns.forEach((element, index) => {
+    const ListName = element.tableName;
+    console.log(ListName);
 
-    const filterCardData = items.filter((element)=>{
-      if(element.status === index){
-        return true;
-      }
-    })
-    console.log('filterCardData',filterCardData);
+    createList(ListName, index);
 
-    filterCardData.forEach((element,itemIndex)=>{
-      const dataEntryList = $(`<div class='ui-widget-content draggable' data-index=${itemIndex}><li class="list-group-item">${element.title}</li></div>`);
-      $(`#list${index}`).append(dataEntryList);
-    })
+    //if items is defined (not null or undefined) and in item.length  we say if it's an array it shuld not be 0
+    if (items && items.length) {
 
-    });
-  
+      // return items which matches example index=0 in same array
+      const filterCardData = items.filter((element) => {
+        if (element.title && element.status === index) {
+          return true;
+        }
+      })
+      console.log('filterCardData', filterCardData);
+
+      filterCardData.forEach((element, itemIndex) => {
+        const dataEntryList = $(`<div class='ui-widget-content draggable' data-index=${itemIndex}><li class="list-group-item">${element.title}</li></div>`);
+        $(`#list${index}`).append(dataEntryList);
+      })
+    }
+
+  });
+
+  dragAndDrop()
+
 }
 
-function createList(ListName, tableIndexNum){
-  
-    const list = $(`<div class="col-sm-4 col-md-6 table droppable m-4" data-column=${tableIndexNum}>
+function createList(ListName, tableIndexNum) {
+
+  const list = $(`<div class="col-sm table droppable" data-column=${tableIndexNum}>
           <div class='dropZone'>
-              <div class="card" style="width: 18rem;">
-                  <div class="card-header">
+              <div class="card">
+                  <div class="card-header" style="width: 18rem">
                   ${ListName}
                   </div class='ulOutside'>
                   <ul  id ='list${tableIndexNum}' class="list-group list-group-flush">
                   
                   </ul>
               </div>
-              <div class="input-group" style="width: 18rem;">
+              <div class="input-group">
                   <input type="text" id='dataEntry${tableIndexNum}' class="form-control" placeholder="Add a card" aria-label="Recipient's username" aria-describedby="button-addon2">
                   <div class="input-group-append">
                   <button class="btn btn-outline-success addItem" type="button" data-column=${tableIndexNum}>Add</button>
@@ -73,22 +80,55 @@ function createList(ListName, tableIndexNum){
           </div>
       </div>
     `);
-  
-    //doesnt make an empty list if the the list doesn't have a name
-    if(ListName === ''){
-      alert('Please enter list name before continue');
-      list.children().remove();
+
+  //doesnt make an empty list if the the list doesn't have a name
+  if (!ListName) { // this if means if listName doesnt have any value
+    alert('Please enter list name before continue');
+    list.children().remove();
+  }
+
+  $('.row').append(list)
+  $('#listNameEntry').val('');
+
+}
+
+function dragAndDrop(){
+  $('.draggable').draggable();
+  $('.draggable').draggable({ revert: true });
+
+
+  $('.droppable').droppable({
+    drop: (event, ui) => {
+      console.log(event);
+      console.log(ui);
+
+      // console.log('column target', event.target.dataset.column);
+
+      //get the data of the dragged item
+      let dragItemContent = $(ui.draggable[0].dataset);
+
+      // Since HTMLElement.dataset returns a DOM string map, 
+      // the only way I found is to convert it into an native object by using below code
+      // dragItemContent = JSON.parse(JSON.stringify(dragItemContent));
+
+      //get index number of dragged item
+      const indexNum = dragItemContent[0].index;
+      tableEntry[indexNum].status = parseInt(event.target.dataset.column);
+      console.log('parseint', parseInt(event.target.dataset.column));
+      console.log(tableEntry);
+
+      //Prepend dragged item in to the droppable list
+      $(`#list${event.target.dataset.column}`).prepend(ui.draggable);
+
+
     }
-  
-    $('.row').append(list)
-    $('#listNameEntry').val('');
-  
+  });
 }
 
 $(() => {
 
-   loadTrelloApp();
- 
+  loadTrelloApp();
+
   function drawTableToDom() {
 
     // console.log('event',e);
@@ -99,65 +139,46 @@ $(() => {
     // below steps is for saving Trello-app info into the local storage (Trello-app-column)
     const JSONstring = JSON.stringify(tableIndex);
     window.localStorage.setItem('Trello-app-column', JSONstring);
-    
-    createList(ListName,tableIndexNum);
+
+    createList(ListName, tableIndexNum);
   }
 
   $('#button-addon').on('click', drawTableToDom);
 
-  
+
 
   function moveItemToList(e) {
-    // console.log(e);
-    // console.log(e.target.dataset.column);
+    console.log(e);
+    console.log(e.target.dataset.column);
 
     event.preventDefault();
     const dataEntry = $(`#dataEntry${e.target.dataset.column}`).val();
-    cardData.push({ title: dataEntry, status: parseInt(e.target.dataset.column)});
+    tableEntry.push({ title: dataEntry, status: parseInt(e.target.dataset.column) });
+    
 
     //store data into local storage (Trello-app-item)
-    const JSONstring = JSON.stringify(cardData);
+    const JSONstring = JSON.stringify(tableEntry);
     window.localStorage.setItem('Trello-app-items', JSONstring);
 
-    const dataIndexNum = cardData.length - 1;
+    const dataIndexNum = tableEntry.length - 1;
     $('input').val('');
     // console.log(dataIndexNum);
-    console.log(cardData);
+    console.log(tableEntry);
 
     const dataEntryList = $(`<div class='ui-widget-content draggable' data-index=${dataIndexNum}><li class="list-group-item">${dataEntry}</li></div>`);
     $(`#list${e.target.dataset.column}`).append(dataEntryList);
 
-    $('.draggable').draggable();
-    $('.draggable').draggable({ revert: true });
+    if(!dataEntry){
+      alert('Please enter card name before continue');
+    dataEntryList.last().remove();
+    }
 
+    dragAndDrop();
 
-    $('.droppable').droppable({
-      drop: (event, ui) => {
-        // console.log(event);
-        // console.log(ui);
-
-        // console.log('column target', event.target.dataset.column);
-
-        //get the data of the dragged item
-        let dragItemContent = $(ui.draggable[0].dataset);
-
-        // Since HTMLElement.dataset returns a DOM string map, 
-        // the only way I found is to convert it into an native object by using below code
-        // dragItemContent = JSON.parse(JSON.stringify(dragItemContent));
-
-        //get index number of dragged item
-        const indexNum = dragItemContent[0].index;
-        cardData[indexNum].status = parseInt(event.target.dataset.column);
-        console.log(cardData);
-
-        //Prepend dragged item in to the droppable list
-        $(`#list${event.target.dataset.column}`).prepend(ui.draggable);
-
-
-      }
-    });
   }
 
   $(document).on("click", '.addItem', moveItemToList);
+
+  
 
 })
